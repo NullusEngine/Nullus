adb03b4f2b4713500ea5ffb46d4a1dd696f9529f
辰洋 <68892939+fuchenyang01@users.noreply.github.com>
2024-10-15 11:32:18 +0800

    Gameobject单独序列化

--- Runtime/Engine/Serialize/GameobjectSerialize.h
+++ Runtime/Engine/Serialize/GameobjectSerialize.h
@@ -1,24 +1,9 @@
 #pragma once
-
 #include "Serialize/IJsonHandler.h"
 
 namespace NLS
 {
-class CommonValueHandler final : public IJsonHandler
-{
-public:
-    virtual void SerializeImpl(const ObjectView& data, json& output) const;
-
-    // 反序列化有两种情况：
-    // 一是在input中包含了对象类型，例如该对象是Json中的顶层对象。
-    // 二是该对象是行内对象，即是某对象的成员变量，此时对象的类型由外部调用者提供
-    virtual void DeserializeImpl(const ObjectView& data, const json& input) const;
-
-    // 返回值越小匹配程度越高
-    virtual uint32_t CalcMatchLevel(const Type& type, bool isPointer) const;
-};
-
-class CommonPointerHandler final : public IJsonHandler
+class GameObjectSerializeHandler final : public IJsonHandler
 {
 public:
     virtual void SerializeImpl(const ObjectView& data, json& output) const;
